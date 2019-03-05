2ff955e154cc0d1c00004d102fd51e1b548a5f10
SUKHVIR.GILL <SUKHVIR.GILL@MKM870-D-04D3.SeneDS.senecacollege.ca>
2019-03-05 11:34:55 -0500

    Test 3

---
+++ Test Suites/New Test Suite2.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>efeccf99-6565-4424-ad63-8b296fe5d2dd</testSuiteGuid>
+</TestSuiteEntity>
